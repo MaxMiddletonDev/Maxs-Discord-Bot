1ed56f8eed41f6c56463b25ba6d2301f322e5a9c
x3trn <18751409-x3trn@users.noreply.replit.com>
2025-11-12 22:19:51 +0000

    Initial commit

---
+++ cogs/fun.py
@@ -0,0 +1,50 @@
+import discord
+from discord.ext import commands
+import aiohttp
+import random
+
+async def get_kanye_quote():
+  async with aiohttp.ClientSession() as session:
+    async with session.get("https://api.kanye.rest") as response:
+      json_data = await response.json()
+      quote = json_data['quote'] + " - Kanye West"
+      return (quote)
+
+class Fun(commands.Cog):
+    
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def kanyequote(self, ctx):
+        quote = await get_kanye_quote()
+        await ctx.send(quote)
+
+
+    @commands.command()
+    async def hello(self, ctx):
+        await ctx.send(f"Hello {ctx.author.mention}!")
+
+    @commands.command()
+    async def flipacoin(self, ctx, member: discord.Member, user_call: str):
+      call = user_call.lower()
+      
+      if call not in ['heads', 'tails']:
+          await ctx.send(f"Sorry, {ctx.author.mention}, you must call 'heads' or 'tails'")
+          return
+
+      options = ['heads', 'tails']
+      result = random.choice(options)
+
+      message = f"Coin flip for {member.mention} and {ctx.author.mention}!\n\n"
+      message += f"The coin landed on: **{result.upper()}**!\n\n"
+
+      if call == result:
+          message += f"{ctx.author.name} wins and {member.name} loses!"
+      else:
+          message += f"{ctx.author.name} lost and {member.name} wins!"
+        
+      await ctx.send(message)
+      
+async def setup(bot):
+    await bot.add_cog(Fun(bot))
---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+import os
+import aiohttp 
+from keep_alive import keep_alive
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def setup_hook():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py') and not filename.startswith('__'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
+
+@bot.event
+async def on_ready():
+  print(f'Stand Ready for {bot.user} Arrival')
+
+keep_alive()
+bot.run(os.getenv('TOKEN'))
